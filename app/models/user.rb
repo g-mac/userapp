4187103b1b6376a3199b5184e76fe58aedd4ec47
g-mac <simonmayrshofer@hotmail.com>
2016-06-03 17:33:00 +0200

    configure user model accordingly (add validations and password encryption using bcrypt)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+  before_save :encrypt_password
+
+  # EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  # validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+  # validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+  validates :email, :presence => true, :uniqueness => true
+  validates_length_of :password, :in => 6..20, :on => :create
+  validates :password, :confirmation => true #password_confirmation attr
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
 end
